0c015fc468b8bb5f36d2cfeab21a0cf3da4465dd
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-07-08 11:18:34 +0530

    Added : Code for basic logical problem

---
+++ BasicProblem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class BasicProblem
+    {
+        public static void addTwoNumber(int a, int b, int d)
+        {
+            int c = (a + b) * d;
+            Console.WriteLine(c);
+        }
+
+        public static string CtoF(double celsius)
+        {
+            double fahrenheit;
+
+            if (celsius < -273.15)
+            {
+                return "Temperature below  zero!";
+            }
+
+            fahrenheit = celsius * 1.8 + 32;
+
+            return $"T = {fahrenheit}F";
+        }
+        public static void EleOperations(int a, int b)
+        {
+            int add = a + b;
+            int sub = a - b;
+            int mul = a * b;
+            double div = a / (double)b;
+            Console.WriteLine(add + " " + sub + " " + mul + " " + div);
+        }
+
+        public static bool isTheResultSame(double a, double b)
+        {
+            return a == b;
+        }
+
+        public static void SwapTwoNumbers(int a, int b)
+        {
+            Console.WriteLine("Your number before swap : " + a + ", " + b);
+            int temp = b;
+            b = a;
+            a = temp;
+            Console.WriteLine("Your number after swap " + a + ", " + b);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Logical Problems Using c#");
+            // This all program is basic program.
+            BasicProblem.addTwoNumber(20, 25, 30);
+            Console.WriteLine(BasicProblem.CtoF(-300));
+            BasicProblem.EleOperations(20, 30);
+            Console.WriteLine(BasicProblem.isTheResultSame(2 * 2, 2 + 2));
+            BasicProblem.SwapTwoNumbers(10, 15);
         }
     }
 }
