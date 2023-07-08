afa2e46e9e29efd2ec9f1ba406f52722cbe1c3c3
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-07-08 11:13:10 +0530

    Added : Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Logical Problems Using c#");
+        }
+    }
+}
